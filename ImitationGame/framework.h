263f859df7f1095a6d8e453b1e39d0d258c791f1
포도맛 버섯 <skacjstk0818@gmail.com>
2022-06-08 22:45:16 +0900

    [2022.06.08] C++17 버전교체 및 define으로 std::byte 해제

--- ImitationGame/framework.h
+++ ImitationGame/framework.h
@@ -22,6 +22,7 @@
 
 #include "targetver.h"
 #define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+#define _HAS_STD_BYTE 0		// C++17 std::byte를 활성화 해제 https://blog.cxx.kr/archives/390
 // Windows 헤더 파일
 #include <windows.h>
 #include <shellapi.h>		// Drag, explorer 등등
