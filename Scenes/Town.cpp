5e3a822c54fccaa9a6d89a8d3398739f7b495e7b
포도맛 버섯 <skacjstk0818@gmail.com>
2022-06-29 21:28:43 +0900

    [2022.06.29] Player 테스트코드 삽입

--- Scenes/Town.cpp
+++ Scenes/Town.cpp
@@ -1,5 +1,6 @@
 #include "./ImitationGame/framework.h"
 #include "./Object/Player.h"
+#include "./Physics/Collider.h"
 #include "Town.h"
 
 Town::Town()
@@ -10,17 +11,21 @@ Town::Town()
 	wstring strShader = SHADER_FOLDER; strShader += L"Texture.hlsl";
 	
 	// 317, 181
-	backGround_ = new Texture(strImage, strShader,0, 520, 318, 702);
-	backGround_->SetScale(6.056782f * WSCALEX, 5.9668508f * WSCALEY);
+	backGround_ = new Texture(strImage, strShader,0, 521, 318, 702);
+	backGround_->SetScale(6.0377358f * WSCALEX, 5.9668508f * WSCALEY);
 	backGround_->SetPosition(0.0f, 0.0f);
 	printf("%f %f\n", backGround_->GetTextureRealSize().x, backGround_->GetTextureRealSize().y);
 
 	tempPlayer_ = new Player();
+	_tempFloor = new Collider();
+	_tempFloor->SetScale(800.0f, 1.0f);
+	_tempFloor->SetPosition(0.0f, -300.0f);
 }
 
 Town::~Town()
 {
 	SAFE_DELETE(backGround_);
+	SAFE_DELETE(tempPlayer_);
 }
 
 void Town::Update()
@@ -30,15 +35,16 @@ void Town::Update()
 	P = CAMERA->GetProjectionMatrix();
 
 	CAMERA->Update(V, P);
-
 	backGround_->Update(V, P);
 	tempPlayer_->Update(V, P);
+	_tempFloor->Update(V, P);
 }
 
 void Town::Render()
 {
 	backGround_->Render();
 	tempPlayer_->Render();
+	_tempFloor->Render();
 }
 
 void Town::ChangeScene()
