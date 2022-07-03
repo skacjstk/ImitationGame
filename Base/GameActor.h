f31d1467aca0b3d6cd26333dc91a3b45197ea0b9
포도맛 버섯 <skacjstk0818@gmail.com>
2022-07-04 00:15:00 +0900

    [2022.07.04] InventoryToggle 가상함수 추가

--- Base/GameActor.h
+++ Base/GameActor.h
@@ -3,8 +3,6 @@
 class GameActor : public GameObject
 {
 protected:
-	float gravity_ = 0.0f;	// Actor들에게 적용되는 매 프레임마다 아래로 떨어지는 변화량
-	bool isGround_ = false;	// Actor 자신이 땅에 닿았는지 확인할 변수
 	class Collider* pCollider_ = nullptr;
 public:
 	GameActor() {};
@@ -14,8 +12,9 @@ public:
 	virtual void RightMove() {};
 	virtual void Jump() {};
 	virtual void Move() {};	// 파생용: 0630 기준 Left,Right에서 정해준 방향으로 Move를 호출하는 역할. 
-	virtual void Idle() = 0;		// IDLE과 Attack은 꼭 있어야 함.
-	virtual void Attack() = 0;
+	virtual void Idle() {};		// IDLE과 Attack은 꼭 있어야 함.
+	virtual void Attack() {};
+	virtual void InventoryToggle() {};
 public:
 	Collider* GetCollider() { return pCollider_; }
 };
