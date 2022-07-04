46d1722587579212ee5b4752affb1e166cf062a0
포도맛 버섯 <skacjstk0818@gmail.com>
2022-07-05 00:14:14 +0900

    [2022.07.05] 인벤토리 슬롯변경 구현

--- Object/Inventory.cpp
+++ Object/Inventory.cpp
@@ -1,6 +1,8 @@
 #include "ImitationGame/framework.h"
 #include "Object/Slot.h"
 #include "./Object/Weapons/MeleeWeapons/ShortSword.h"
+#include "./InputHandler/InventoryInputHandler.h"
+#include "./InputHandler/InputHandler.h"
 #include "Inventory.h"
 
 Inventory::Inventory()
@@ -60,13 +62,21 @@ Inventory::Inventory()
 			(-50.0f * WSCALEY) - (slotgap_ * (i / slotX_)) - numY * (i / slotX_));
 	}
 	// (-295.0f * WSCALEY) + (slotgap_ * (i / slotX_)) + numY * (i / slotX_)
+	SetInvenTabGroup();
 	// 테스트코드
+	equipSlots_[0]->SetItem(new ShortSword());
 	itemSlots_[0]->SetItem(new ShortSword());
+	inputHandler_ = (InputHandler*)new InventoryInputHandler();
 }
 
 Inventory::~Inventory()
 {
 	SAFE_DELETE(invenBaseImage_);
+	for (int i = 0; i < _countof(itemSlots_); ++i)
+		SAFE_DELETE(itemSlots_[i]);
+	for (int i = 0; i < _countof(equipSlots_); ++i)
+		SAFE_DELETE(equipSlots_[i]);
+	SAFE_DELETE(inputHandler_);
 }
 
 void Inventory::Update(Matrix V, Matrix P)
@@ -80,6 +90,9 @@ void Inventory::Update(Matrix P)
 	Matrix V = CAMERA->GetAbsoluteViewMatrix();
 	invenBaseImage_->SetScale(this->GetScale());
 	invenBaseImage_->Update(V, P);
+
+	InputUpdate();
+	
 	for (int i = 0; i < _countof(itemSlots_); ++i) {
 		itemSlots_[i]->Update(V, P);
 	}
@@ -102,6 +115,129 @@ void Inventory::Render()
 }
 
 void Inventory::Reset()
+{
+
+}
+// 슬롯 간 교체가 일어났을 때 수행함.
+void Inventory::ChangeSlot(Slot** dragSlot, Slot** dropSlot)
+{
+	Item* temp = (*dragSlot)->GetItem();
+	(*dragSlot)->SetItem((*dropSlot)->GetItem());
+	(*dropSlot)->SetItem(temp);
+
+//	Slot* temp = new Slot();
+//	temp->SetItem((*dragSlot)->GetItem());
+//	(*dragSlot)->SetItem((*dropSlot)->GetItem());
+//	(*dropSlot)->SetItem(temp->GetItem());
+}
+//void Inventory::UpdateEquip(){} 는 맨아래에
+
+void Inventory::InputUpdate()
+{
+	vector<Command*> commandQueue = (*inputHandler_).handleInput();	// 진짜로 큐는 아니야
+	for (Command* command : commandQueue) {
+		GameActor* tempActor = this;
+		command->execute(*tempActor);
+	}
+}
+
+void Inventory::SetInvenTabGroup()
+{
+	int itemCount = _countof(itemSlots_);
+	int equipCount = _countof(equipSlots_);
+	// 우 설정
+	itemSlots_[itemCount - 1]->GetSlotButton()->SetRightTabGroup(equipSlots_[0]->GetSlotButton());
+	equipSlots_[equipCount - 1]->GetSlotButton()->SetRightTabGroup(itemSlots_[0]->GetSlotButton());
+
+	for (int i = 0; i < equipCount - 1; ++i) {
+		equipSlots_[i]->GetSlotButton()->SetRightTabGroup(equipSlots_[i + 1]->GetSlotButton());
+	}
+	for (int i = 0; i < itemCount - 1; ++i) {
+		itemSlots_[i]->GetSlotButton()->SetRightTabGroup(itemSlots_[i + 1]->GetSlotButton());
+	}
+
+	// 좌 설정
+	equipSlots_[0]->GetSlotButton()->SetLeftTabGroup(itemSlots_[itemCount - 1]->GetSlotButton());
+	itemSlots_[0]->GetSlotButton()->SetLeftTabGroup(equipSlots_[equipCount - 1]->GetSlotButton());
+
+	for (int i = equipCount -1; i > 0; --i) {
+		equipSlots_[i]->GetSlotButton()->SetLeftTabGroup(equipSlots_[i - 1]->GetSlotButton());
+	}
+	for (int i = itemCount - 1; i > 0; --i) {
+		itemSlots_[i]->GetSlotButton()->SetLeftTabGroup(itemSlots_[i - 1]->GetSlotButton());
+	}
+	// Next: 상하 설정
+
+	itemSlots_[0]->GetSlotButton()->SetActivate(true);
+}
+// 클릭한 아이템이 없으면 드래그, 있으면 드롭 후 SlotChange 호출. Next: 이 무식한 방법을 해결하기
+void Inventory::ItemDragAndDrop()
+{
+	if (dragIndex_.slotType == 0)
+		Drag();	
+	else 
+		Drop();
+}
+void Inventory::Drag()
+{
+	int itemCount = _countof(itemSlots_);
+	int equipCount = _countof(equipSlots_);
+	
+	for (int i = 0; i < itemCount; ++i) {
+		if (itemSlots_[i]->GetSlotButton()->IsActivate()) {
+			dragIndex_.slotType = 3;
+			dragIndex_.slotIndex = i;
+			return;
+		}
+	}
+	for (int i = 0; i < equipCount; ++i) {
+		if (equipSlots_[i]->GetSlotButton()->IsActivate()) {
+			dragIndex_.slotType = 2;
+			dragIndex_.slotIndex = i;
+			return;
+		}
+	}
+	// 대충 뭔가를 지정해주기
+}
+void Inventory::Drop()
+{
+	bool isUpdateItem = false;
+	if (dragIndex_.slotType != 3 || dropIndex_.slotType != 3)	// 둘중 하나라도 장비된거 변경한거면 UpdateEquip 하기
+		isUpdateItem = true;
+
+	int itemCount = _countof(itemSlots_);
+	int equipCount = _countof(equipSlots_);
+
+	bool isCheck = false;
+	for (int i = 0; i < itemCount; ++i) {
+		if (itemSlots_[i]->GetSlotButton()->IsActivate()) {
+			dropIndex_.slotType = 3;
+			dropIndex_.slotIndex = i;
+			isCheck = true;
+			break;
+		}
+	}
+	for (int i = 0; i < equipCount; ++i) {
+		if (isCheck == true)
+			break;
+		if (equipSlots_[i]->GetSlotButton()->IsActivate()) {
+			dropIndex_.slotType = 2;
+			dropIndex_.slotIndex = i;
+			break;
+		}
+	}
+
+	// 일단 아이템칸 옮기기 해보자 장비칸 신경쓰지말고
+	ChangeSlot(&itemSlots_[dragIndex_.slotIndex], &itemSlots_[dropIndex_.slotIndex]);
+//	std::swap(itemSlots_[dragIndex_.slotIndex], itemSlots_[dropIndex_.slotIndex]);
+
+
+ 	if (isUpdateItem)
+	//	UpdateEquip();
+
+	dropIndex_ = dragIndex_ = { 0,0 };
+}
+void Inventory::UpdateEquip(Slot* changedSlot, int slotIndex)
 {
 
 }
--- Object/Inventory.h
+++ Object/Inventory.h
@@ -2,20 +2,39 @@
 class Inventory : public GameActor
 {
 private:
+	struct swapIndex {
+		int slotType;	// 1이 equip, 2가 액세서리, 3이 item
+		int slotIndex;
+	};
 	Texture* invenBaseImage_ = nullptr;
+	GameActor* inventoryOwner_ = nullptr;
+	class InputHandler* inputHandler_ = nullptr;
 	// 이 안에 작은 imgui를 만드는게 좋으려나?
 	class Slot* equipSlots_[8] = { nullptr, };
 	class Slot* itemSlots_[15] = { nullptr, };	// 첫 
 	float slotgap_ = 10.0f;
 	int slotX_ = 5;	// 가로 슬롯의 갯수: 얘는 슬롯 배치할때 연산용으로 씀
 	int slotY_ = 3;	// 세로 슬롯의 갯수
-
+	struct swapIndex dragIndex_ = { 0,0 };
+	struct swapIndex dropIndex_ = { 0,0 };
+	class Slot* dragSlot_ = nullptr;
+	class Slot* dropSlot_ = nullptr;
 public: // 생성자 & 소멸자
 	Inventory();
 	~Inventory();
 public:	// 공개 인스턴스 변수 
-	void Update(Matrix V, Matrix P);	// 사용하지 않음.
+	void Update(Matrix V, Matrix P) override;	// 사용하지 않음.
 	void Update(Matrix P);
-	void Render();
-	void Reset();
+	void Render() override;
+	void Reset() override;
+	void ChangeSlot(Slot** dragSlot, Slot** dropSlot);	// 슬롯을 바꿔치기함.	
+	// ChangeSlot에서 장비 아이템에 변화가 생겼을 경우(악세포함) 장비를 다시 업데이트하는 함수
+	void UpdateEquip(Slot* changedSlot, int slotIndex);		
+	void SetInventoryOwner(GameActor* actor) { inventoryOwner_ = actor; }	// 플레이어와 인벤토리간의 연결고리를 위한 작업
+private:
+	void InputUpdate();
+	void SetInvenTabGroup();
+	void ItemDragAndDrop() override;
+	void Drag();
+	void Drop();
 };
