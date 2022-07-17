02a8b7ee8253465dd6dc6b02e1deebfacc64dac2
포도맛 버섯 <skacjstk0818@gmail.com>
2022-07-18 01:36:16 +0900

    [2022.07.18] 미사용 던전팩토리

---
+++ Scenes/DungeonFactory.cpp
@@ -0,0 +1,8 @@
+#include "./ImitationGame/framework.h"
+#include "Scenes/Room.h"
+#include "DungeonFactory.h"
+
+void DungeonFactory::GenerateDungeon(int floor)
+{
+
+}
---
+++ Scenes/DungeonFactory.h
@@ -0,0 +1,12 @@
+#pragma once
+
+/*
+// Input Assembly 단계 -> Vertex Stage -> Resster Stage -> Pixel Stage -> Out merger 
+IA = 코딩
+VS, RS, PS = Shader Language
+*/
+class DungeonFactory
+{
+public:
+	void GenerateDungeon(int floor);
+};
