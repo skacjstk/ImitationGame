e488f4583d1cb48cbf225551b55eef5bb90d88d9
포도맛 버섯 <skacjstk0818@gmail.com>
2022-07-20 23:10:03 +0900

    [2022.07.20] SkelDog 애니메이션 추가

--- Object/Enemy/SkelDog.cpp
+++ Object/Enemy/SkelDog.cpp
@@ -1,42 +1,115 @@
 #include "ImitationGame/framework.h"
+#include "Physics/Collider.h"
 #include "State/State.h"
 #include "State/SkelDogState.h"
 #include "State/IdleState.h"
 #include "State/RunState.h"
+#include "State/AttackState.h"
 #include "SkelDog.h"
 
 SkelDog::SkelDog()
 {
+	SetActive(false);
 	state_[0] = new IdleState();
 	state_[1] = new RunState();
-	currentState_ = state_[0];
-	
+	state_[2] = new AttackState();
+	currentState_ = state_[static_cast<int>(stateEnum_)];	
+
+	wstring strImage = IMAGE_FOLDER; strImage += L"Monster/SkelDog/SkelDogDie.png";
+	wstring strShader = SHADER_FOLDER; strShader += L"Texture.hlsl";
+
+	_animation = new Animation(strImage, strShader);
+
+	// IDLE
+	{
+		AnimationClip* pClip = new AnimationClip(AnimationClip::eState::Loop);
+
+		for (int i = 0; i <= 4; ++i) {
+			strImage = IMAGE_FOLDER; strImage += L"Monster/SkelDog/SkelDogIdle" + to_wstring(i) + L".png";
+			pClip->AddFrame(_animation->GetTexture(), strImage, 0, 0, 0.1f);
+
+		}
+		_animation->AddClip(pClip);
+	}
+	// RUN
+	{
+		AnimationClip* pClip = new AnimationClip(AnimationClip::eState::Loop);
+
+		for (int i = 0; i <= 6; ++i) {
+			strImage = IMAGE_FOLDER; strImage += L"Monster/SkelDog/SkelDogRun" + to_wstring(i) + L".png";
+			pClip->AddFrame(_animation->GetTexture(), strImage, 0, 0, 0.1f);
+
+		}
+		_animation->AddClip(pClip);
+	}
+	// Attack	// Run 0번 재탕
+	{
+		AnimationClip* pClip = new AnimationClip(AnimationClip::eState::EndStay);
+		strImage = IMAGE_FOLDER; strImage += L"Monster/SkelDog/SkelDogRun0.png";
+		pClip->AddFrame(_animation->GetTexture(), strImage, 0, 0, 0.1f);		
+		_animation->AddClip(pClip);
+	}
+	// 배율 설정 및 충돌체 할당
+	pCollider_ = new Collider();
+	_animation->SetScale(6.0f * WSCALEX, 6.0f * WSCALEY);
 }
 
 SkelDog::~SkelDog()
 {
+	SAFE_DELETE(_animation);
+	SAFE_DELETE(currentState_);
+	for(int i=0;i<_countof(state_);++i)
+		SAFE_DELETE(state_[i]);
+	SAFE_DELETE(pCollider_);
 }
 
 void SkelDog::Update(Matrix V, Matrix P)
 {
+	if (IsActive() == false)
+		return;
 	// IDLE, RUN, ATTACK(RUN 0 번 재탕으로 뛰어오르기)
+	GroundCheck();
 	StateUpdate();
+	_animation->SetPlay(static_cast<int>(stateEnum_));
+	_animation->SetPosition(GetPosition());
+	_animation->SetRotation(GetRotation());
+	_animation->Update(V, P);
 
-
+	pCollider_->SetPosition(GetPosition());
+	pCollider_->SetRotation(GetRotation());
+	pCollider_->Update(V, P);
 }
 
 void SkelDog::Render()
 {
+	if (IsActive() == false)
+		return;
+	_animation->Render();
+	pCollider_->Render();
 }
 
 void SkelDog::Reset()
 {
+	SetActive(true);
+	SetScale(3.0f * WSCALEX, 3.0f * WSCALEY);
+	_animation->SetScale(GetScale());
+	pCollider_->SetScale(_animation->GetTextureRealSize());
+	chaseTarget_ = (GameActor*)OBJECTMANAGER->FindObject("player");
+	this->SetPosition(chaseTarget_->GetPosition());
 }
 
 void SkelDog::StateUpdate()
 {
 	currentState_->SwitchState(*this);
 	currentState_->Action(*this);
+}
+
+void SkelDog::CollisionCheck()
+{
+}
+
+void SkelDog::AttackCheck()
+{
 }
 
 void SkelDog::LeftMove()
--- Object/Enemy/SkelDog.h
+++ Object/Enemy/SkelDog.h
@@ -2,18 +2,32 @@
 class SkelDog : public GameActor
 {
 public:
+	enum class DogState	
+	{
+		IDLE = 0,
+		RUN = 1,
+		ATTACK = 2,
+		WAIT = 3	// 모션은 IDLE의 1번을 공유할 예정
+	};
+	DogState stateEnum_ = DogState::IDLE;	// 이놈은 index 대용. static_cast로 currentState_에 주입함.
 	class SkelDogState* currentState_ = nullptr;
-	class SkelDogState* state_[2] = { nullptr, };
+	class SkelDogState* state_[3] = { nullptr, };
+//	Animation* _animation = nullptr;	// GameActor에 넣음.
+	class GameActor* chaseTarget_ = nullptr;	// 이걸 모든 enemy가 가지고 있게 하고싶은데... 공통을 못뽑겠네 Next
+	float moveSpeed = 400.0f;
+	float jumpSpeed = 500.0f;
 private:
-	Animation* _animation = nullptr;
+
 public: // 생성자 & 소멸자
 	SkelDog();
 	~SkelDog();
 public:	// 공개 인스턴스 변수 
-	void  Update(Matrix V, Matrix P) override;
-	void  Render() override;
-	void  Reset() override;
+	void Update(Matrix V, Matrix P) override;
+	void Render() override;
+	void Reset() override;
 	void StateUpdate();
+	void CollisionCheck();
+	void AttackCheck();
 
 	void LeftMove() override;
 	void RightMove() override;
@@ -22,4 +36,7 @@ public:	// 공개 인스턴스 변수
 	void Idle() override;		// IDLE과 Attack은 꼭 있어야 함.
 	void Attack() override;
 
+public:	//Getter
+	bool IsGround() { return isGround_; }
+	bool IsConflicted() { return isConflicted_; }
 };
