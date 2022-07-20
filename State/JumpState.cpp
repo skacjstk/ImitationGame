3b0af1d39802f5fc013cf6cd2545d4f4a67cfc75
포도맛 버섯 <skacjstk0818@gmail.com>
2022-07-21 01:52:00 +0900

    [2022.07.21] SkelDog JumpState 구현

---
+++ State/JumpState.cpp
@@ -0,0 +1,27 @@
+#include "ImitationGame/framework.h"
+#include "Object/Enemy/SkelDog.h"
+#include "State/SkelDogState.h"
+#include "State/RunState.h"
+#include "JumpState.h"
+
+void JumpState::SwitchState(SkelDog& actor)
+{
+	// 점프 수행 후, IDLE 로 변경
+	if (actor.IsGround() == true) {
+		actor.stateEnum_ = SkelDog::DogState::IDLE;
+		actor.currentState_ = actor.state_[static_cast<int>(actor.stateEnum_)];
+		actor.currentState_->Enter(actor);
+	}
+	//actor.state_->enter(*this);	// 책에 있던 내용	
+}
+
+void JumpState::Action(SkelDog& actor)
+{
+	// 하는게 없는데, 움직이지도 않고...
+	actor.Jump();
+}
+
+void JumpState::Enter(SkelDog& actor)
+{
+	// 히트판정 제거
+}
---
+++ State/JumpState.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Object/Enemy/SkelDog.h"
+#include "State/SkelDogState.h"
+class JumpState : public SkelDogState
+{
+public: // 持失切 & 社瑚切
+	JumpState() {};
+	~JumpState() {};
+public:
+	void SwitchState(class SkelDog& actor) override;
+	void Action(class SkelDog& actor) override;
+	void Enter(class SkelDog& actor) override;
+};
