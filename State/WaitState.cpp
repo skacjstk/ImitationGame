ff4e3f80219240611adf2736154f27d868e1165e
포도맛 버섯 <skacjstk0818@gmail.com>
2022-07-21 01:52:07 +0900

    [2022.07.21] SkelDog WaitState 구현

---
+++ State/WaitState.cpp
@@ -0,0 +1,25 @@
+#include "ImitationGame/framework.h"
+#include "Object/Enemy/SkelDog.h"
+#include "State/SkelDogState.h"
+#include "State/RunState.h"
+#include "WaitState.h"
+
+void WaitState::SwitchState(SkelDog& actor)
+{
+	// 일정 거리 이내라면, RUN State로 변경
+	if (time_ > 1.0f) {
+		actor.stateEnum_ = SkelDog::DogState::ATTACK;
+		actor.currentState_ = actor.state_[static_cast<int>(actor.stateEnum_)];
+		actor.currentState_->Enter(actor);
+	}
+}
+
+void WaitState::Action(SkelDog& actor)
+{
+	time_ += TIMEMANAGER->Delta();
+}
+
+void WaitState::Enter(SkelDog& actor)
+{
+	time_ = 0.0f;
+}
---
+++ State/WaitState.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Object/Enemy/SkelDog.h"
+#include "State/SkelDogState.h"
+class WaitState : public SkelDogState
+{
+private:
+	float time_ = 0.0f;
+public: // 持失切 & 社瑚切
+	WaitState() {};
+	~WaitState() {};
+public:
+	void SwitchState(class SkelDog& actor) override;
+	void Action(class SkelDog& actor) override;
+	void Enter(class SkelDog& actor) override;
+};
