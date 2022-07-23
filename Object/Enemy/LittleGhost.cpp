309833c0ca55c412888683d3cac54887d8cd2613
포도맛 버섯 <skacjstk0818@gmail.com>
2022-07-23 23:18:10 +0900

    [2022.07.23] LittleGhost 추가

---
+++ Object/Enemy/LittleGhost.cpp
@@ -0,0 +1,142 @@
+#include "ImitationGame/framework.h"
+#include "Physics/Collider.h"
+#include "State/State.h"
+#include "State/LittleGhostState/LittleGhostState.h"
+#include "State/LittleGhostState/IdleStateLG.h"
+#include "State/LittleGhostState/MoveStateLG.h"
+#include "State/LittleGhostState/AttackStateLG.h"
+#include "LittleGhost.h"
+
+LittleGhost::LittleGhost()
+{
+	SetActive(false);
+	state_[0] = new MoveStateLG();
+	state_[1] = new AttackStateLG();
+	currentState_ = state_[static_cast<int>(stateEnum_)];
+
+	wstring strImage = IMAGE_FOLDER; strImage += L"Monster/LittleGhost/LittleGhost0.png";
+	wstring strShader = SHADER_FOLDER; strShader += L"Texture.hlsl";
+
+	_animation = new Animation(strImage, strShader);
+
+	// MOVE
+	{
+		AnimationClip* pClip = new AnimationClip(AnimationClip::eState::Loop);
+
+		for (int i = 0; i <= 5; ++i) {
+			strImage = IMAGE_FOLDER; strImage += L"Monster/LittleGhost/LittleGhost" + to_wstring(i) + L".png";
+			pClip->AddFrame(_animation->GetTexture(), strImage, 0, 0, 20, 20, 0.06f);
+
+		}
+		_animation->AddClip(pClip);
+	}
+	// Attack
+	{
+		AnimationClip* pClip = new AnimationClip(AnimationClip::eState::Loop);
+
+		for (int i = 0; i <= 2; ++i) {
+			strImage = IMAGE_FOLDER; strImage += L"Monster/LittleGhost/LittleGhostAttack" + to_wstring(i) + L".png";
+			pClip->AddFrame(_animation->GetTexture(), strImage, 0, 0, 20, 20, 0.06f);
+		}
+		_animation->AddClip(pClip);
+	}
+	// 배율 설정 및 충돌체 할당
+	pCollider_ = new Collider();
+}
+
+LittleGhost::~LittleGhost()
+{
+	SAFE_DELETE(_animation);
+	SAFE_DELETE(currentState_);
+	for (int i = 0; i < _countof(state_); ++i)
+		SAFE_DELETE(state_[i]);
+	SAFE_DELETE(pCollider_);
+}
+
+void LittleGhost::Update(Matrix V, Matrix P)
+{
+	if (IsActive() == false)
+		return;
+	// IDLE, RUN, ATTACK(RUN 0 번 재탕으로 뛰어오르기)
+	StateUpdate();
+	// GroundCheck 를 한정 조건화 함. ( Attack 중에는 땅검사 안함 )
+	// Gravity도 마찬가지 ( 지금 구현하는 LittleGhost 처럼 충돌이 발생하지 않는 애들도 있어서 )
+
+	// SetPlay를 각 상태쪽으로 넘김.
+	_animation->SetPosition(GetPosition());
+	_animation->SetRotation(GetRotation());
+	_animation->Update(V, P);
+
+	pCollider_->SetPosition(GetPosition());
+	pCollider_->SetRotation(GetRotation());
+	pCollider_->SetScale(_animation->GetTextureRealSize());
+	pCollider_->SetPivot(_animation->GetPivot());
+	pCollider_->Update(V, P);
+}
+
+void LittleGhost::Render()
+{
+	if (IsActive() == false)
+		return;
+	_animation->Render();
+	pCollider_->Render();
+}
+
+void LittleGhost::Reset()
+{
+	SetActive(true);
+	SetScale(3.0f * WSCALEX, 3.0f * WSCALEY);
+	_animation->SetScale(GetScale());
+	chaseTarget_ = (GameActor*)OBJECTMANAGER->FindObject("player");
+	this->SetPosition(chaseTarget_->GetPosition());
+	currentState_->Enter(*this);
+}
+
+void LittleGhost::StateUpdate()
+{
+	currentState_->SwitchState(*this);
+	currentState_->Action(*this);
+}
+
+void LittleGhost::AttackCheck()
+{
+	// 겹쳐져 있으면 공격 성공, 타겟의 무적시간이 0 이면서
+	if (chaseTarget_->GetImmuneFrame() <= 0 && Collider::IntersectAABB(pCollider_, chaseTarget_->GetCollider()))
+	{
+		chaseTarget_->Attacked();
+	}
+}
+
+void LittleGhost::Move(float& radian)
+{
+	// chaseTarget_ 추적
+	printf("각도:%f\n", radian * 180 / PI);
+	float sign = copysign(1, radian);	// copysign 부호 비트 추출 어쨋든 양수면 1, 음수면 -1
+	ModifyPosition(cosf(radian) * moveSpeed * TIMEMANAGER->Delta(),
+		sinf(radian) * moveSpeed * TIMEMANAGER->Delta());	// Next: -1 곱해야 할 수 있음.
+
+
+	printf("sign: %f\n", sign);
+	// 각도는 정상
+	sign -= 1.0f;	// 양수 2, 음수 0
+	SetRotation(0.0f, sign * 90.0f, 0.0f);
+}
+
+void LittleGhost::Move()
+{
+	printf("LittleGhost는 distance를 받는 개별 Move를 사용함");
+}
+
+void LittleGhost::Attack()
+{
+	printf("LittleGhost는 dX를 받는 개별 Attack을 사용함");
+}
+
+// 점프와 같지만 dX는 딱 한번만 연산함.
+void LittleGhost::Attack(float& radian, float& dX, float& dY)
+{	
+	ModifyPosition(cosf(radian) * moveSpeed * TIMEMANAGER->Delta(),
+		sinf(radian) * moveSpeed * TIMEMANAGER->Delta());	// Next: -1 곱해야 할 수 있음.
+
+	AttackCheck();
+};
---
+++ Object/Enemy/LittleGhost.h
@@ -0,0 +1,35 @@
+#pragma once
+class LittleGhost : public GameActor
+{
+public:
+	enum class LittleGhostStateEnum {
+		MOVE,	// 플레이어 주변 떠돌기
+		ATTACK	// 가까워지면 조금 더 빠르게 돌진
+	};
+	LittleGhostStateEnum stateEnum_ = LittleGhostStateEnum::MOVE;	// 이놈은 index 대용. static_cast로 currentState_에 주입함.
+	class LittleGhostState* currentState_ = nullptr;
+	class LittleGhostState* state_[2] = { nullptr, };
+
+	class GameActor* chaseTarget_ = nullptr;	// 이걸 모든 enemy가 가지고 있게 하고싶은데... 공통을 못뽑겠네 Next
+	float moveSpeed = 300.0f;
+	float jumpSpeed = 100.0f;
+public: // 생성자 & 소멸자
+	LittleGhost();
+	~LittleGhost();
+public:	// 공개 인스턴스 변수 
+	void Update(Matrix V, Matrix P) override;
+	void Render() override;
+	void Reset() override;
+	void StateUpdate();
+	void AttackCheck();
+
+	void Move() override;
+	void Move(float& radian);
+	void Attack() override;
+	void Attack(float& radian, float& dX, float& dY);
+
+public:	//Getter
+	bool IsGround() { return isGround_; }
+	bool IsConflicted() { return isConflicted_; }
+	Animation* GetAnimation() { return _animation; }
+};
