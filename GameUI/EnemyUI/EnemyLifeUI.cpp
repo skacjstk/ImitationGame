5c1ae8dc50f6324b141c47bc21d5f2a3cef90a53
포도맛 버섯 <skacjstk0818@gmail.com>
2022-07-27 01:17:49 +0900

    [2022.07.27] 적 하단 체력바 프레임

---
+++ GameUI/EnemyUI/EnemyLifeUI.cpp
@@ -0,0 +1,71 @@
+#include "ImitationGame/framework.h"
+#include "EnemyLifeUI.h"
+
+EnemyLifeUI::EnemyLifeUI()
+{
+	SetActive(false);
+	wstring strImage = IMAGE_FOLDER; strImage += L"UI/PlayerLifeBase 1.png";
+	wstring strShader = SHADER_FOLDER; strShader += L"Texture.hlsl";
+	imageBase_ = new Texture(strImage, strShader);
+	strImage = IMAGE_FOLDER; strImage += L"UI/PlayerLifeBack.png";	//74 x 16
+	imageBack_ = new Texture(strImage, strShader);
+	strImage = IMAGE_FOLDER; strImage += L"UI/PlayerLifeBar.png";	//49 x 10
+	lifeBar_ = new Texture(strImage, strShader);
+}
+
+EnemyLifeUI::~EnemyLifeUI()
+{
+	SAFE_DELETE(imageBase_);
+	SAFE_DELETE(imageBack_);
+}
+
+void EnemyLifeUI::Update(Matrix V, Matrix P)
+{
+	imageBack_->Update(V, P);
+	lifeBar_->Update(V, P);
+	imageBase_->Update(V, P);
+}
+
+void EnemyLifeUI::Render()
+{
+	if (IsActive() == false)
+		return;
+	imageBack_->Render();
+	lifeBar_->Render();
+	imageBase_->Render();
+	DirectWrite::RenderTextCenter(hpDesc, hpDescPos.x, hpDescPos.y, 255, 255, 255, font_size);
+}
+void EnemyLifeUI::Reset()
+{
+	imageBase_->SetScale(GetScale());
+	imageBack_->SetScale(GetScale());
+	imageBase_->SetPosition(GetPosition());
+	imageBack_->SetPosition(GetPosition());
+	modifyScale = lifeBarBaseScale = GetScale();
+
+	lifeBar_->SetScale(lifeBarBaseScale);
+
+	float hpInversePosToPivot = lifeBar_->GetTextureRealSize().x * 0.5f;
+	Vector2 tempRS = imageBase_->GetTextureRealSize();
+
+	lifeBar_->_pivot = Vector3(-hpInversePosToPivot, 0.0f, 0.0f);
+	lifeBar_->SetPosition(GetPosition().x + (tempRS.x * 0.128f) - hpInversePosToPivot, GetPosition().y);
+	// lifeBar 폰트 위치 및 크기 조정.
+
+	float temp = ((((WSCALEX + WSCALEY) / 2) - 1.0f) / 2) + 1.0f;
+	font_size = font_size * temp;
+	hpDescPos = lifeBar_->GetPosition();
+	hpDescPos.x += -lifeBar_->_pivot.x;
+	hpDescPos.y += font_size * 0.5f;
+	SetActive(true);
+}
+
+void EnemyLifeUI::UpdateLifeBar(int currentHp, int maxHP)
+{
+	// Next: Enemy용으로 머리 위에 띄우는거 하기.(보스제외)
+	modifyScale.x = (float)currentHp / (float)maxHP;
+	modifyScale.x *= lifeBarBaseScale.x;
+	lifeBar_->SetScale(modifyScale);
+	hpDesc = to_wstring(currentHp) + L" / " + to_wstring(maxHP);
+	lifeBar_->_pivot = Vector3(-lifeBar_->GetTextureRealSize().x * 0.5f, 0.0f, 0.0f);
+}
---
+++ GameUI/EnemyUI/EnemyLifeUI.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "GameUI/BaseUI.h"
+#include "./Render/Texture.h"
+class EnemyLifeUI : public BaseUI
+{
+	class Texture* imageBase_ = nullptr;
+	class Texture* imageBack_ = nullptr;
+	Texture* lifeBar_ = nullptr;
+	Vector2 lifeBarBaseScale = Vector2(0.0f, 0.0f);	
+	Vector2 modifyScale = Vector2(0.0f, 0.0f);	// x scale을 조정해 체력바를 표현할 것
+	// 체력 바에 표시할 숫자
+	wstring hpDesc = L"";
+	Vector2 hpDescPos = Vector2(0.0f, 0.0f);
+	float font_size = 45.0f;
+public: // 생성자 & 소멸자
+	EnemyLifeUI();
+	~EnemyLifeUI();
+public:	// 공개 인스턴스 변수 
+	void  Update(Matrix V, Matrix P) override;
+	void  Render() override;
+	void  Reset() override;
+public:
+	Vector2 GetTextureRealSize() { return imageBase_->GetTextureRealSize(); }	// dashUI 위치 보정용
+	void  UpdateLifeBar(int currentHp, int maxHP);
+};
