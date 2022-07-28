d2888a124c83567446c7137ac9b189f3e71339ac
포도맛 버섯 <skacjstk0818@gmail.com>
2022-07-28 23:52:41 +0900

    [2022.07.28] 다음층 등반 이벤트 구현

---
+++ Event/RiseNextFloorEvent.cpp
@@ -0,0 +1,9 @@
+#include "ImitationGame/framework.h"
+#include "Event.h"
+#include "RiseNextFloorEvent.h"
+
+void RiseNextFloorEvent::execute()
+{
+	printf("다음층 오르기: 호출 전 해당 씬에서 이름을 설정해줄 것\n");
+	SCENEMANAGER->ChangeScene(SCENEMANAGER->GetCurrentScene()->GetName());
+}
---
+++ Event/RiseNextFloorEvent.h
@@ -0,0 +1,6 @@
+#pragma once
+class RiseNextFloorEvent : public Event
+{
+public: // 持失切 & 社瑚切
+	void execute() override;
+};
