8c51d65197e8991989f975730deda9e69235dc98
포도맛 버섯 <skacjstk0818@gmail.com>
2022-08-02 02:53:38 +0900

    [2022.08.02] EventHandler 반영

--- System/EventHandler.cpp
+++ System/EventHandler.cpp
@@ -2,6 +2,10 @@
 #include "Event/EnterDungeonEvent.h"
 #include "Event/SwapItemEvent.h"
 #include "Event/RiseNextFloorEvent.h"
+#include "Event/MoveRoomDown.h"
+#include "Event/MoveRoomLeft.h"
+#include "Event/MoveRoomUp.h"
+#include "Event/MoveRoomRight.h"
 //#include "Event/Event.h"
 #include "EventHandler.h"
 
@@ -10,8 +14,11 @@ EventHandler::EventHandler()
 	eventMap.insert(std::make_pair<wstring, Event*>(L"EnterDungeon", new EnterDungeonEvent()));
 	eventMap.insert(std::make_pair<wstring, Event*>(L"SwapItem", new SwapItemEvent()));
 	eventMap.insert(std::make_pair<wstring, Event*>(L"RiseNextFloor", new RiseNextFloorEvent()));
+	eventMap.insert(std::make_pair<wstring, Event*>(L"MR1", new MoveRoomDown()));
+	eventMap.insert(std::make_pair<wstring, Event*>(L"MR2", new MoveRoomLeft()));
+	eventMap.insert(std::make_pair<wstring, Event*>(L"MR3", new MoveRoomUp()));
+	eventMap.insert(std::make_pair<wstring, Event*>(L"MR4", new MoveRoomRight()));
 }
-
 void EventHandler::Update()
 {
 	while(!eventQueue.empty()){
