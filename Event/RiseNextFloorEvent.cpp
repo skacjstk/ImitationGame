c68ba11d0bf6ba9405116f4061491d4149e73d47
포도맛 버섯 <skacjstk0818@gmail.com>
2022-08-07 23:07:19 +0900

    [2022.08.07] 다음 층 승강 이벤트 완료

--- Event/RiseNextFloorEvent.cpp
+++ Event/RiseNextFloorEvent.cpp
@@ -6,5 +6,5 @@ void RiseNextFloorEvent::execute()
 {
 	printf("다음층 오르기: 호출 전 해당 씬에서 이름을 설정해줄 것\n");
 	SCENEMANAGER->GetCurrentScene()->ExitScene();
-	SCENEMANAGER->ChangeScene(SCENEMANAGER->GetCurrentScene()->GetName());
+	SCENEMANAGER->ChangeScene(SCENEMANAGER->GetCurrentScene()->GetNextName());
 }
